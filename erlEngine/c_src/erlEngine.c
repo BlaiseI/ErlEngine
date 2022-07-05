c3d28d113ce275684a37edf0806215ce6d3e1823
BlaiseI <BlaiseWorlds@gmail.com>
2022-07-05 13:47:19 +0200

    test finished

--- erlEngine/c_src/erlEngine.c
+++ erlEngine/c_src/erlEngine.c
@@ -5,7 +5,7 @@
 #include <SDL_image.h>
 
 #define WINDOW_WIDTH (960)
-#define WINDOWN_HEIGHT (540)
+#define WINDOW_HEIGHT (540)
 
 #define SCROLL_SPEED (300)
 #define SPEED (300)
@@ -32,7 +32,7 @@ ERL_NIF_TERM test(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
     }
     printf("SDL initiated, no error\n");
 
-    SDL_Window *win = SDL_CreateWindow("Hello, world !", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOWN_HEIGHT,0);
+    SDL_Window *win = SDL_CreateWindow("Hello, world !", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT,0);
     if(!win){
         printf("error : %s, quit sdl", SDL_GetError());
         SDL_Quit();
@@ -48,7 +48,7 @@ ERL_NIF_TERM test(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
         return atom_nok;
     }
 
-    SDL_Surface *surface = IMG_Load("/home/blaise/Documents/heart.png");
+    SDL_Surface *surface = IMG_Load("/home/blaise/Documents/heart2.png");
     if(!surface){
         printf("error : %s, close window and quit sdl", SDL_GetError());
         SDL_DestroyRenderer(rend);
@@ -72,22 +72,107 @@ ERL_NIF_TERM test(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
     SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
     
     float x_pos = (WINDOW_WIDTH - dest.w) /2;
-    float x_pos = (WINDOW_HEIGHT- dest.h) /2;
-    float x_vel = SPEED;
-    float y_vel = SPEED;
+    float y_pos = (WINDOW_HEIGHT- dest.h) /2;
+    float x_vel = 0;
+    float y_vel = 0;
 
-    float y_pos = WINDOWN_HEIGHT;
+    int up = 0;
+    int down = 0;
+    int left = 0;
+    int right = 0;
 
     int close_requested = 0;
     while(!close_requested){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+
+            switch (event.type){
+            case SDL_QUIT:
+                close_requested = 1;
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.scancode){
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    up = 1;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    down = 1;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    left = 1;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    right = 1;
+                    break;
+                default:
+                    break;
+                }
+                break;
+            case SDL_KEYUP:
+                switch (event.key.keysym.scancode){
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    up = 0;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    down = 0;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    left = 0;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    right = 0;
+                    break;
+                default:
+                    break;
+                }
+                break;
+            }
+        }
+
+        int mouse_x, mouse_y;
+        int buttons = SDL_GetMouseState(&mouse_x, &mouse_y);
+
+        x_vel = y_vel = 0;
+        if(up && !down) y_vel=-SPEED;
+        if(!up && down) y_vel= SPEED;
+        if(left && !right) x_vel=-SPEED;
+        if(right && !left) x_vel= SPEED;
+
+        if (x_pos <= 0)
+            x_pos = 0;
+        if (y_pos <= 0)
+            y_pos = 0;
+        if (x_pos >= (WINDOW_WIDTH - dest.w))
+            x_pos = WINDOW_WIDTH - dest.w;
+        if (y_pos >= (WINDOW_HEIGHT - dest.h))
+            y_pos = WINDOW_HEIGHT - dest.h;
+
+
+        // Update the sprite position
+        y_pos += y_vel / 60;
+        x_pos += x_vel / 60;
+
+        // Set the positions in the spriteRect
+        dest.x = (int)x_pos;
+        dest.y = (int)y_pos;
+
+        // Clear the window
         SDL_RenderClear(rend);
-        dest.y = (int) y_pos;
+
+        // Draw the image to the window
         SDL_RenderCopy(rend, tex, NULL, &dest);
         SDL_RenderPresent(rend);
 
-        y_pos -= (float) SCROLL_SPEED /60;
-
-        SDL_Delay(1000/60);
+        // Wait
+        SDL_Delay(1000 / 60);
     }
 
     //SDL_RenderClear(rend);
